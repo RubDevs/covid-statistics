700bcb6887760a5a9911b6a7fe1cd2d3ad1c4610
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-05-26 18:23:13 -0500

    Travis CI added

--- server/index.js
+++ server/index.js
@@ -15,6 +15,6 @@ app.use(
   graphqlHTTP({ schema, rootValue: resolvers, graphiql: true })
 );
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("app listening");
 });
