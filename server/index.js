d2eda54263acfe17e453fcf7440266bf04994cd5
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-05-25 20:01:24 -0500

    graphql endpoint added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const path = require("path");
+
+const schema = require("./graphQL/schema");
+const resolvers = require("./graphQL/");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
+app.use(
+  "/graphql",
+  graphqlHTTP({ schema, rootValue: resolvers, graphiql: true })
+);
 
 app.listen(3000, () => {
   console.log("app listening");
