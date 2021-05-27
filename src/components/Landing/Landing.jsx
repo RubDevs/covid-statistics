bf7b4a2b4611ce80907884552f6027a462c34026
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-05-26 21:10:45 -0500

    Landing Page basic mockup created. Styling will be added later.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,29 @@
 export const Landing = () => (
-  <section className="py-5">
-    <h1>Landing</h1>
-  </section>
+  <section className="row">
+      <div className="col-sm-12 col-12">
+        <section className="hero-container text-center">
+          <h1>Graphs and statistics related to covid</h1>
+          <p>Verify data related to covid, occupied beds, recovered and deceased patients in real time.</p>
+          <button>Start looking</button>
+        </section>
+      </div>
+      <div className="col-12">
+        <section className="maps-container">
+          <h2>Maps</h2>
+          <p>Our APIs, SDKs, and live updating map data give developers tools to build better mapping, navigation, and search experiences across platforms.</p>
+          <button>Start looking</button>
+          {/* <img src="" alt="" /> */}
+        </section>
+      </div>
+      <div className="col-md-12">
+        <section className="seach-img">
+        
+        </section>
+        <section className="search-container">
+          <h2>Search</h2>
+          <p>Our APIs, SDKs, and live updating map data give developers tools to build better mapping, navigation, and search experiences across platforms.</p>
+          <button>Start looking</button>
+        </section>
+      </div>
+    </section>
 );
