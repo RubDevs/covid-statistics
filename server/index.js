e607273eb4740a55dfc443ea64fbb0f775225534
Rubén Hernández <69823633+RubDevs@users.noreply.github.com>
2021-05-29 20:13:57 -0500

    register endpoint added (#35)

--- server/index.js
+++ server/index.js
@@ -5,15 +5,18 @@ const path = require("path");
 
 const schema = require("./graphQL/schema");
 const resolvers = require("./graphQL/");
+const apiUsers = require("./components/user/network");
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 app.use(
   "/graphql",
   graphqlHTTP({ schema, rootValue: resolvers, graphiql: true })
 );
+apiUsers(app);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("app listening");
