f11ffa0e871c4be72ddb17db53bd3b0c87e15bc1
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-05-30 20:56:04 -0500

    Column container added to the button in order to center it for mobile. Last media querie pending.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,29 +1,35 @@
 export const Landing = () => (
   <section className="row">
-      <div className="col-12 col-sm-12">
-        <section className="hero-container text-center">
-          <h1>Graphs and statistics related to covid</h1>
-          <p>Verify data related to covid, occupied beds, recovered and deceased patients in real time.</p>
-          <button>Start looking</button>
-        </section>
-      </div>
-      <div className="col-12 col-sm-12">
-        <section className="maps-container">
-          <h2>Maps</h2>
-          <p>Our APIs, SDKs, and live updating map data give developers tools to build better mapping, navigation, and search experiences across platforms.</p>
-          <button>Start looking</button>
-          {/* <img src="" alt="" /> */}
-        </section>
-      </div>
-      <div className="col-12 col-sm-12">
-        <section className="seach-img">
-        
-        </section>
-        <section className="search-container">
-          <h2>Search</h2>
-          <p>Our APIs, SDKs, and live updating map data give developers tools to build better mapping, navigation, and search experiences across platforms.</p>
-          <button>Start looking</button>
-        </section>
-      </div>
-    </section>
+    <div className="col-12 col-sm-12">
+      <section className="hero-container text-center">
+        <h1>Graphs and statistics related to covid</h1>
+        <p>Verify data related to covid, occupied beds, recovered and deceased patients in real time.</p>
+        <button>Start looking</button>
+      </section>
+    </div>
+    <div className="col-12 col-sm-12">
+      <section className="maps-container">
+        <h2>Maps</h2>
+        <p>Our APIs, SDKs, and live updating map data give developers tools to build better mapping, navigation, and search experiences across platforms.</p>
+        <div className="col-sm-12 btn-container">
+          <a href="">
+            <button>Start looking</button>
+          </a>
+        </div>
+      </section>
+    </div>
+    <div className="col-12 col-sm-12">
+      <section className="seach-img">
+      </section>
+      <section className="search-container">
+        <h2>Search</h2>
+        <p>Our APIs, SDKs, and live updating map data give developers tools to build better mapping, navigation, and search experiences across platforms.</p>
+        <div className="col-sm-12 btn-container">
+          <a href="">
+            <button>Start looking</button>
+          </a>
+        </div>
+      </section>
+    </div>
+  </section>
 );
