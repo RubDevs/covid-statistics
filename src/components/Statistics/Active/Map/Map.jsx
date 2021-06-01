9c5e6495f2107000116b20806bf7541511949ece
williboss <santiago_alza@hotmail.com>
2021-05-31 22:14:29 -0500

    Add apollo and graphql libraries and connect with api

--- src/components/Statistics/Active/Map/Map.jsx
+++ src/components/Statistics/Active/Map/Map.jsx
@@ -1,5 +1,17 @@
+// Import components
+import { LastCovidDataQuery } from '../../../../graphql/LastCovidDataQuery';
+
+export const Component = ({ loading, error, data }) => {
+  console.log(data);
+  return (
+    <section className="py-5">
+      <h1>Statistics Active Map</h1>
+    </section>
+  )
+};
+
 export const StatisticsActiveMap = () => (
-  <section className="py-5">
-    <h1>Statistics Active Map</h1>
-  </section>
-);
+  <LastCovidDataQuery>
+    {Component}
+  </LastCovidDataQuery>
+)
