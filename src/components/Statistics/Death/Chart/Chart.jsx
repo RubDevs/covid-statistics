43afadf76f114162ee78fe4396e331d05a9ae596
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-06-03 15:19:57 -0500

    Media queries & more styles added to the charts. Finished. Request to API will continue in order to fetch the data.

--- src/components/Statistics/Death/Chart/Chart.jsx
+++ src/components/Statistics/Death/Chart/Chart.jsx
@@ -3,12 +3,25 @@ import Chart from 'react-apexcharts'
 
 const options = {
   chart: {
-    id: "basic-bar"
+    id: "basic-bar",
+    toolbar: {
+      show: true,
+      offsetX: 5,
+      offsetY: 2,
+      tools: {
+        download: true,
+        selection: true,
+        zoom: true,
+        zoomin: true,
+        zoomout: true,
+        reset: true | '<img src="/static/icons/reset.png" width="20">',
+      }
+    }
   },
   xaxis: {
     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
   },
-  theme :{
+  theme: {
     palette: 'palette4'
   },
   title: {
@@ -17,15 +30,35 @@ const options = {
     margin: 10,
     floating: false,
     style: {
-      fontSize:  '18px',
-      fontWeight:  'bold',
-      fontFamily: "Noto Sans JP",
+      fontSize: '14px',
+      fontFamily: "Nanum Gothic",
       color: '#033a75',
     },
   },
   stroke: {
     curve: 'smooth',
-  }
+    width: 2,
+  },
+  responsive: [{
+    breakpoint: 500,
+    options: {
+      chart: {
+        toolbar: {
+          show: true,
+          tools: {
+            download: false,
+            selection: false,
+            zoom: false,
+            zoomin: true,
+            zoomout: false,
+            pan: false,
+            reset: true | '<img src="/static/icons/reset.png" width="20">',
+          }
+        },
+        width: '290'
+      },
+    }
+  }]
 }
 
 const series = [
@@ -38,12 +71,12 @@ const series = [
 export const StatisticsDeathChart = () => (
   <div className="container-fluid">
     <section className="row">
-      <div className="col-9">
+      <div className="col-10 col-xl-6">
         <Chart
           options={options}
           series={series}
           type="line"
-          width="700"
+          width="640"
         />
       </div>
     </section>
