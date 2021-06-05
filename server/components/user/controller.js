fb7b94bac02cd4ef875153cbab7139752bfd11f7
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-06-05 11:19:09 -0500

    tests added

--- server/components/user/controller.js
+++ server/components/user/controller.js
@@ -1,4 +1,9 @@
-const User = require("../../models/user");
+const config = require("../../config");
+let User;
+if (!config.dev) {
+  const User = require("../../models/user");
+}
+
 const field = "email";
 
 //Dependency injection
---
+++ server/mocks/ratiosMock.js
@@ -0,0 +1,22 @@
+const ratiosMock = [
+  {
+    CreationDate: "2021-05-25",
+    Code: "MEX",
+    Name: "Mexico",
+    Deaths: "235000",
+    TotalPopulation: "120000000",
+    Ratio_per_100_000: "0.68",
+    Total_Ratio: "0.08",
+  },
+  {
+    CreationDate: "2021-05-25",
+    Code: "COL",
+    Name: "Colombia",
+    Deaths: "235000",
+    TotalPopulation: "120000000",
+    Ratio_per_100_000: "0.68",
+    Total_Ratio: "0.08",
+  },
+];
+
+module.exports = ratiosMock;
