37d5c5d832fa940f1c167b30c10c8aa89e723dc1
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-06-09 22:30:58 -0500

    google tokens validated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 const boom = require("@hapi/boom");
+const validateFacebookToken = require("../utils/validateFacebookToken");
+const validateGoogle = require("../utils/validateGoogleToken");
 
 /**
  * Verifies if a token is valid
  */
-function middleware(req, res, next) {
+async function middleware(req, res, next) {
   const bearerToken = req.headers["authorization"];
   if (!bearerToken) {
     return next(boom.unauthorized("Token required"));
@@ -17,10 +19,23 @@ function middleware(req, res, next) {
 
   jwt.verify(token, config.jwt.secret, (error, decoded) => {
     if (!error) {
-      next();
-    } else {
-      next(boom.unauthorized("Invalid token"));
+      return next();
     }
   });
+
+  //verify if is google oauth token
+  const isGoogleToken = await validateGoogle(token);
+  if (isGoogleToken) {
+    return next();
+  }
+
+  //verify if is facebook oauth token
+  const isFacebookToken = validateFacebookToken(token);
+  if (isFacebookToken) {
+    return next();
+  }
+  //is not valid
+  next(boom.unauthorized("Invalid token"));
 }
+
 module.exports = middleware;
